2c2dd16044a3569eaca4e040b49eabb2e907acea
VMD121199 <minhducvu1211@gmail.com>
2019-12-19 22:20:31 +0700

    Edit StudentController function Edit Delete, hide CreateAt UpdateAt and DeleteAt in Student View

--- OneTech/Controllers/StudentsController.cs
+++ OneTech/Controllers/StudentsController.cs
@@ -89,6 +89,7 @@ namespace OneTech.Controllers
         {
             if (ModelState.IsValid)
             {
+                student.UpdatedAt = DateTime.Now;
                 _db.Entry(student).State = EntityState.Modified;
                 _db.SaveChanges();
                 return RedirectToAction("Index");
@@ -118,7 +119,11 @@ namespace OneTech.Controllers
         public ActionResult DeleteConfirmed(int id)
         {
             var student = _db.Students.Find(id);
-            if (student != null) _db.Students.Remove(student);
+            if (student != null && ModelState.IsValid)
+            {
+                student.DeletedAt = DateTime.Now;
+                _db.Entry(student).State = EntityState.Modified;
+            };
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
