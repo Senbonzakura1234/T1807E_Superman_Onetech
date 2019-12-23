7905487ddec32574c8568ca7d8f50fa2606f19d9
Đức Lê <rabbithunter0502>
2019-12-23 13:12:13 +0700

    feature#_Create-class-controller/add-attendance-model

---
+++ OneTech/Models/Attendance.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OneTech.Models
+{
+    public class Attendance
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public double OwedCash { get; set; }
+        public int OwedPushUp { get; set; }
+        public int PenaltyLevel { get; set; }
+        public int Penalty { get; set; }
+        public PenaltyEnum PenaltyType { get; set; }
+        public enum PenaltyEnum
+        {
+            Cash = 0,
+            Pushup = 1
+        }
+    }
+}
